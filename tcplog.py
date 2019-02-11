0a78d4eeef7e66838b6adeb611521c6a6e011268
Shobhit Bhosure <38807205+shobhit99@users.noreply.github.com>
2019-02-11 11:22:55 +0530

    Update tcplog.py

--- tcplog.py
+++ tcplog.py
@@ -1,8 +1,18 @@
 import re
+import subprocess
+import sys
+from threading import Thread
+
+def op():
+	subprocess.call("tshark -r output -T fields -e frame.time_epoch -e eth.src -e tcp.srcport -e tcp.dstport -E header=y -E separator=, -E quote=d -E occurrence=f > output.csv", shell=True)
 
 user_list = ['user11', 'user12', 'user21', 'user22', 'user23', 'user24', 'user31', 'user32', 'user41', 'user42', 'user51', 'user52', 'user61', 'user62', 'user63', 'user64']
 pt_list = [2201, 2202, 2203, 2204, 2205, 2206, 2207, 2208, 2209, 2210, 2211, 2212, 2214, 2215, 2216, 2217]
 userdict = dict(zip(pt_list,user_list))
+
+t = Thread(target=op)
+t.start()
+t.join()
 
 with open("output.csv", "r") as f:
 	data = f.read()
