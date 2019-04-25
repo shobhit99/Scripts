a1c59ffa33a28e2148956aa714fba4e3ae7fadd3
Shobhit Bhosure <38807205+shobhit99@users.noreply.github.com>
2019-04-25 16:01:09 +0530

    Added disk read and write

--- stat.py
+++ stat.py
@@ -10,9 +10,13 @@ mem_used_list = []
 cpu_output_list = []
 network_stat_down = []
 network_stat_up = []
+io_read = []
+io_write = []
 times = []
 def bmon():
 	net_output = subprocess.check_output("bmon -o ascii > net_op",shell=True)
+def iotop():
+	io_output = subprocess.call("iotop -b -d 1 | grep --line-buffered 'Current DISK WRITE' > io_output",shell=True)
 
 def getinkb(val):
 	if val.endswith("KiB"):
@@ -26,6 +30,8 @@ def getinkb(val):
 try:
 	p = Process(target=bmon)
 	p.start()
+	p1 = Process(target=iotop)
+	p1.start()
 	while True:
 		cpu_output = subprocess.check_output("top -b -n1 | grep 'Cpu(s)' | awk '{print $2 + $4}'",shell=True)
 		mem_output = subprocess.check_output("free -m | grep Mem",shell=True)
@@ -49,12 +55,17 @@ except:
 		up = getinkb(values[3])
 		network_stat_up.append(up)
 		network_stat_down.append(down)
+	iodata = open("io_output", "r").read()
+	redata = re.findall(r"Current\sDISK\sREAD:\s+(.*)\s\|\sCurrent\sDISK\sWRITE:\s+(.*)",iodata)
+	for iod in redata:
+		io_read.append(iod[0])
+		io_write.append(iod[1])
 	ts = int(time.time())
 	with open("{}.csv".format(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')), "w") as csvfile:
-		fields = ['Timestamp','Memory Usage','Cpu Usage','Network Down','Network Up']
+		fields = ['Timestamp','Memory Usage','Cpu Usage','Network Down','Network Up','Disk Read','Disk Write']
 		csvwriter = csv.writer(csvfile, delimiter=',')
 		csvwriter.writerow(fields)
 		for i in range(len(mem_used_list)):
-			arr = [times[i],mem_used_list[i],cpu_output_list[i],network_stat_down[i],network_stat_up[i]]
+			arr = [times[i],mem_used_list[i],cpu_output_list[i],network_stat_down[i],network_stat_up[i],io_read[i],io_write[i]]
 			csvwriter.writerow(arr)
 	
